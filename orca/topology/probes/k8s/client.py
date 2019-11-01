6e870c0cdfd5c4502c74b0d2724c62d8ba4af5d9
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2019-11-01 23:26:34 +0100

    Refactor K8S Watch
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/k8s/client.py
+++ orca/topology/probes/k8s/client.py
@@ -16,22 +16,27 @@ class Watch(object):
     def __init__(self, resource_api, handler):
         self._resource_api = resource_api
         self._handler = handler
+        self._handler_func_mapping = self._build_handler_func_mapping()
 
     def run(self):
         resource_watch = watch.Watch()
         for event in resource_watch.stream(self._resource_api):
             self._handle_event(event)
 
+    def _build_handler_func_mapping(self):
+        return {
+            'ADD': self._handler.on_add,
+            'UPDATE': self._handler.on_update,
+            'DELETE': self._handler.on_delete
+        }
+
     def _handle_event(self, event):
         event_type = event['type']
         obj = event['object']
-        if event_type == 'ADD':
-            self._handler.on_add(obj)
-        elif event_type == 'UPDATE':
-            self._handler.on_update(obj)
-        elif event_type == 'DELETE':
-            self._handler.on_delete(obj)
-        else:
+        try:
+            handler_func = self._handler_func_mapping[event_type]
+            handler_func(obj)
+        except KeyError:
             raise Exception("Unknown event type: %s" % event_type)
 
 
