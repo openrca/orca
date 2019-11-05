a125f1e24a2827e6434e5ea91e71fc7590202c93
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2019-11-05 18:11:47 +0100

    Extract basic deployment properties
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/k8s/deployment.py
+++ orca/topology/probes/k8s/deployment.py
@@ -1,10 +1,24 @@
 from orca.topology.probes.k8s import probe
+from orca.topology.probes.k8s import client as k8s_client
+from orca.common import logger
+
+log = logger.get_logger(__name__)
 
 
 class DeploymentProbe(probe.K8SProbe):
 
     def run(self):
-        while True:
-            print("fetching deployments..")
-            import time
-            time.sleep(1)
+        resource_api = self._client.list_pod_for_all_namespaces
+        handler = DeploymentHandler(self._graph)
+        log.info("Starting K8S watch on resource: deployment")
+        k8s_client.Watch(resource_api, handler).run()
+
+
+class DeploymentHandler(probe.K8SHandler):
+
+    def _extract_properties(self, obj):
+        id = obj.metadata.uid
+        properties = {}
+        properties['name'] = obj.metadata.name
+        properties['namespace'] = obj.metadata.namespace
+        return (id, properties)
