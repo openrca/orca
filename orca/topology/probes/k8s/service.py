55b998c7dddf57aea523e9fe5d4cb885aaf83df4
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-01-21 18:59:29 +0100

    Add service synchronizer
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/k8s/service.py
+++ orca/topology/probes/k8s/service.py
@@ -1,6 +1,9 @@
 from orca.common import logger
 from orca.k8s import client as k8s_client
+from orca.topology.probes import fetcher
+from orca.topology.probes import synchronizer as sync
 from orca.topology.probes.k8s import extractor
+from orca.topology.probes.k8s import fetcher as k8s_fetcher
 from orca.topology.probes.k8s import probe
 
 log = logger.get_logger(__name__)
@@ -9,8 +12,14 @@ log = logger.get_logger(__name__)
 class ServiceProbe(probe.Probe):
 
     def run(self):
-        log.info("Starting K8S watch on resource: service")
+        log.info("Starting K8S sync on resource: service")
         extractor = ServiceExtractor()
+        graph_fetcher = fetcher.GraphFetcher(self._graph, 'service')
+        upstream_fetcher = k8s_fetcher.FetcherFactory.get_fetcher(self._client, 'service', extractor)
+        synchronizer = sync.Synchronizer(self._graph, graph_fetcher, upstream_fetcher)
+        synchronizer.synchronize()
+        log.info("Finished K8S sync on resource: service")
+        log.info("Starting K8S watch on resource: service")
         handler = probe.KubeHandler(self._graph, extractor)
         watch = k8s_client.ResourceWatch(self._client.CoreV1Api(), 'service')
         watch.add_handler(handler)
@@ -18,6 +27,9 @@ class ServiceProbe(probe.Probe):
 
 
 class ServiceExtractor(extractor.Extractor):
+
+    def extract_kind(self, entity):
+        return 'service'
 
     def extract_properties(self, entity):
         properties = {}
