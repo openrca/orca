528e8427898df7810e56a5f5f70143cf2802864e
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-01-21 11:00:17 +0100

    Move shared logic to base linker class
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/k8s/linker.py
+++ orca/topology/probes/k8s/linker.py
@@ -1,44 +1,7 @@
-import abc
-
-from orca.graph import graph
-from orca.common import logger
 from orca.topology.probes import linker
 
-log = logger.get_logger(__name__)
 
-
-class Linker(linker.Linker, abc.ABC):
-
-    def __init__(self, graph, kind_a, indexer_a, kind_b, indexer_b):
-        super().__init__(graph, kind_a, kind_b)
-        self._indexer_a = indexer_a
-        self._indexer_b = indexer_b
-
-    def _get_new_links(self, node):
-        if node.kind == self.kind_a:
-            return self._get_ab_links(node)
-        else:
-            return self._get_ba_links(node)
-
-    def _get_ab_links(self, node_a):
-        links = []
-        for node_b in self._indexer_b.get_all():
-            if self._are_linked(node_a, node_b):
-                link = graph.Graph.create_link({}, node_a, node_b)
-                links.append(link)
-        return links
-
-    def _get_ba_links(self, node_b):
-        links = []
-        for node_a in self._indexer_a.get_all():
-            if self._are_linked(node_a, node_b):
-                link = graph.Graph.create_link({}, node_a, node_b)
-                links.append(link)
-        return links
-
-    @abc.abstractmethod
-    def _are_linked(self, node_a, node_b):
-        """Determines whether two graph nodes are interconnected."""
+class Linker(linker.Linker):
 
     def _match_namespace(self, node_a, node_b):
         return node_a.properties.namespace == node_b.properties.namespace
--- orca/topology/probes/linker.py
+++ orca/topology/probes/linker.py
@@ -38,41 +38,65 @@ class GraphListener(graph.EventListener):
 
 class Linker(abc.ABC):
 
-    def __init__(self, graph, kind_a, kind_b):
+    def __init__(self, graph, kind_a, indexer_a, kind_b, indexer_b):
+        self.kind_a = kind_a
+        self.kind_b = kind_b
         self._graph = graph
-        self._kind_a = kind_a
-        self._kind_b = kind_b
-
-    @property
-    def kind_a(self):
-        return self._kind_a
-
-    @property
-    def kind_b(self):
-        return self._kind_b
+        self._indexer_a = indexer_a
+        self._indexer_b = indexer_b
 
     def link(self, node):
-        current_links = Linker._build_link_lookup(self._get_current_links(node))
-        new_links = Linker._build_link_lookup(self._get_new_links(node))
-        for link_id, link in new_links.items():
-            if link_id not in current_links:
-                self._graph.add_link(link)
-            else:
-                self._graph.update_link(link)
-        for link_id, link in current_links.items():
-            if link_id not in new_links:
-                self._graph.delete_link(link)
+        links_in_graph = self._build_link_lookup(self._get_links_in_graph(node))
+        upstream_links = self._build_link_lookup(self._get_upstream_links(node))
 
-    def _get_current_links(self, node):
+        links_in_graph_ids = set(links_in_graph.keys())
+        upstream_links_ids = set(upstream_links.keys())
+
+        links_to_delete_ids = links_in_graph_ids.difference(upstream_links_ids)
+        links_to_update_ids = links_in_graph_ids.difference(links_to_delete_ids)
+        links_to_create_ids = upstream_links_ids.difference(links_in_graph_ids)
+
+        for link_id in links_to_delete_ids:
+            self._graph.delete_link(links_in_graph[link_id])
+
+        for link_id in links_to_update_ids:
+            self._graph.update_link(upstream_links[link_id])
+
+        for link_id in links_to_create_ids:
+            self._graph.add_link(upstream_links[link_id])
+
+    def _get_links_in_graph(self, node):
         target_kind = self._get_target_kind(node)
         return self._graph.get_node_links(node, kind=target_kind)
 
     def _get_target_kind(self, node):
         return self.kind_a if node.kind == self.kind_b else self.kind_b
 
+    def _get_upstream_links(self, node):
+        if node.kind == self.kind_a:
+            return self._get_ab_links(node)
+        else:
+            return self._get_ba_links(node)
+
+    def _get_ab_links(self, node_a):
+        links = []
+        for node_b in self._indexer_b.get_all():
+            if self._are_linked(node_a, node_b):
+                link = graph.Graph.create_link({}, node_a, node_b)
+                links.append(link)
+        return links
+
+    def _get_ba_links(self, node_b):
+        links = []
+        for node_a in self._indexer_a.get_all():
+            if self._are_linked(node_a, node_b):
+                link = graph.Graph.create_link({}, node_a, node_b)
+                links.append(link)
+        return links
+
     @abc.abstractmethod
-    def _get_new_links(self, node):
-        """Returns a snapshot of graph links for a given node."""
+    def _are_linked(self, node_a, node_b):
+        """Determines whether two graph nodes are interconnected."""
 
     @staticmethod
     def _build_link_lookup(links):
