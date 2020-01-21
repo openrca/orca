e58a81514a1dc47ebd019e4ecefc83ddfaace316
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-01-21 18:59:20 +0100

    Add replica set synchronizer
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/k8s/replica_set.py
+++ orca/topology/probes/k8s/replica_set.py
@@ -1,7 +1,9 @@
 from orca.common import logger
 from orca.k8s import client as k8s_client
-from orca.topology.probes.k8s import extractor
 from orca.topology.probes import fetcher
+from orca.topology.probes import synchronizer as sync
+from orca.topology.probes.k8s import extractor
+from orca.topology.probes.k8s import fetcher as k8s_fetcher
 from orca.topology.probes.k8s import linker, probe
 
 log = logger.get_logger(__name__)
@@ -10,8 +12,14 @@ log = logger.get_logger(__name__)
 class ReplicaSetProbe(probe.Probe):
 
     def run(self):
-        log.info("Starting K8S watch on resource: replica_set")
+        log.info("Starting K8S sync on resource: replica_set")
         extractor = ReplicaSetExtractor()
+        graph_fetcher = fetcher.GraphFetcher(self._graph, 'replicaset')
+        upstream_fetcher = k8s_fetcher.FetcherFactory.get_fetcher(self._client, 'replica_set', extractor)
+        synchronizer = sync.Synchronizer(self._graph, graph_fetcher, upstream_fetcher)
+        synchronizer.synchronize()
+        log.info("Finished K8S sync on resource: replica_set")
+        log.info("Starting K8S watch on resource: replica_set")
         handler = probe.KubeHandler(self._graph, extractor)
         watch = k8s_client.ResourceWatch(self._client.ExtensionsV1beta1Api(), 'replica_set')
         watch.add_handler(handler)
@@ -19,6 +27,9 @@ class ReplicaSetProbe(probe.Probe):
 
 
 class ReplicaSetExtractor(extractor.Extractor):
+
+    def extract_kind(self, entity):
+        return 'replicaset'
 
     def extract_properties(self, entity):
         properties = {}
