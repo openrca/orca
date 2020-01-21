ceb16daf61a803840c95a7db42a46be9530dc78a
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-01-21 18:59:08 +0100

    Add deployment synchronizer
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/k8s/deployment.py
+++ orca/topology/probes/k8s/deployment.py
@@ -1,6 +1,9 @@
 from orca.common import logger
 from orca.k8s import client as k8s_client
+from orca.topology.probes import fetcher
+from orca.topology.probes import synchronizer as sync
 from orca.topology.probes.k8s import extractor
+from orca.topology.probes.k8s import fetcher as k8s_fetcher
 from orca.topology.probes.k8s import probe
 
 log = logger.get_logger(__name__)
@@ -9,8 +12,14 @@ log = logger.get_logger(__name__)
 class DeploymentProbe(probe.Probe):
 
     def run(self):
-        log.info("Starting K8S watch on resource: deployment")
+        log.info("Starting K8S sync on resource: deployment")
         extractor = DeploymentExtractor()
+        graph_fetcher = fetcher.GraphFetcher(self._graph, 'deployment')
+        upstream_fetcher = k8s_fetcher.FetcherFactory.get_fetcher(self._client, 'deployment', extractor)
+        synchronizer = sync.Synchronizer(self._graph, graph_fetcher, upstream_fetcher)
+        synchronizer.synchronize()
+        log.info("Finished K8S sync on resource: deployment")
+        log.info("Starting K8S watch on resource: deployment")
         handler = probe.KubeHandler(self._graph, extractor)
         watch = k8s_client.ResourceWatch(self._client.AppsV1Api(), 'deployment')
         watch.add_handler(handler)
@@ -18,6 +27,9 @@ class DeploymentProbe(probe.Probe):
 
 
 class DeploymentExtractor(extractor.Extractor):
+
+    def extract_kind(self, entity):
+        return 'deployment'
 
     def extract_properties(self, entity):
         properties = {}
