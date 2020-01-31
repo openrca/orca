aeda3821e26d5c2ec7923ace295c1f55fd9bd239
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-01-31 11:06:42 +0100

    Add base class for alert extractors
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/probes/extractor.py
+++ orca/topology/probes/extractor.py
@@ -17,8 +17,40 @@ class Extractor(abc.ABC):
 
     @abc.abstractmethod
     def extract_kind(self, entity):
-        """Extracts kind of given entity object."""
+        """Extracts kind from given entity object."""
 
     @abc.abstractmethod
     def extract_properties(self, entity):
         """Extracts properties from given entity object."""
+
+
+class AlertExtractor(Extractor):
+
+    def extract_properties(self, entity):
+        return {
+            'name': self.extract_name(entity),
+            'source_labels': self.extract_source_labels(entity),
+            'status': self.extract_status(entity),
+            'severity': self.extract_severity(entity),
+            'message': self.extract_message(entity)
+        }
+
+    @abc.abstractmethod
+    def extract_name(self, entity):
+        """Extracts name from given entity object."""
+
+    @abc.abstractmethod
+    def extract_source_labels(self, entity):
+        """Extracts source labels from given entity object."""
+
+    @abc.abstractmethod
+    def extract_status(self, entity):
+        """Extracts status from given entity object."""
+
+    @abc.abstractmethod
+    def extract_severity(self, entity):
+        """Extracts severity from given entity object."""
+
+    @abc.abstractmethod
+    def extract_message(self, entity):
+        """Extracts message from given entity object."""
