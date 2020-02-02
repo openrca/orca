af7a84a4243850374cff99c90071aa605ed846d6
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-02-02 12:00:13 +0100

    Switch back default logging level to INFO
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/common/logger.py
+++ orca/common/logger.py
@@ -7,7 +7,7 @@ FORMATTER = logging.Formatter(
 
 def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     logger.addHandler(get_console_handler())
     logger.propagate = False
     return logger
