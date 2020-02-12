6bcf9c5872821560e85d05f8c3ac470e7a1f8db5
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-02-12 12:49:14 +0100

    Refactor Kiali probe
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/topology/infra/kiali/service.py
+++ orca/topology/infra/kiali/service.py
@@ -2,9 +2,6 @@ import time
 
 from orca.topology import probe
 from orca.graph import graph
-from orca.common import logger
-
-log = logger.get_logger(__name__)
 
 
 class Probe(probe.Probe):
@@ -15,46 +12,55 @@ class Probe(probe.Probe):
 
     def run(self):
         while True:
-            self._update_service_links()
-            time.sleep(10)
+            self._synchronize_service_graph()
+            time.sleep(60)
 
-    def _update_service_links(self):
+    def _synchronize_service_graph(self):
         namespaces = self._get_namespaces()
-        service_graph = self._kiali_client.graph_namespaces(namespaces)
+        nodes, edges = self._get_service_graph(namespaces)
+        service_mapping = self._build_service_mapping(nodes)
+        self._synchronize_links(edges, service_mapping)
 
+    def _get_namespaces(self):
+        return [namespace['name'] for namespace in self._kiali_client.list_namespaces()]
+
+    def _get_service_graph(self, namespaces):
+        service_graph = self._kiali_client.graph_namespaces(namespaces)
         node_elements = service_graph['elements']['nodes']
         edge_elements = service_graph['elements']['edges']
+        nodes = [element['data'] for element in node_elements]
+        edges = [element['data'] for element in edge_elements]
+        return (nodes, edges)
 
+    def _build_service_mapping(self, nodes):
         service_mapping = {}
-        for element in node_elements:
-            node = element['data']
+        for node in nodes:
             if node['nodeType'] == 'service':
                 service_mapping[node['id']] = {
                     'name': node['service'],
                     'namespace': node['namespace']}
+        return service_mapping
 
-        for element in edge_elements:
-            edge = element['data']
+    def _synchronize_links(self, edges, service_mapping):
+        for edge in edges:
             source_id = edge['source']
             target_id = edge['target']
-
             source_mapping = service_mapping.get(source_id)
             target_mapping = service_mapping.get(target_id)
-
             if source_mapping and target_mapping:
-                self._link_services(source_mapping, target_mapping)
+                source_node = self._get_service(source_mapping)
+                target_node = self._get_service(target_mapping)
+                if source_node and target_node:
+                    self._link_services(source_node, target_node)
 
-    def _link_services(self, source_mapping, target_mapping):
-        source_matches = self._graph.get_nodes('service', properties=source_mapping)
-        target_matches = self._graph.get_nodes('service', properties=target_mapping)
+    def _get_service(self, mapping):
+        matches = self._graph.get_nodes('service', properties=mapping)
+        if matches:
+            return matches[0]
 
-        if source_matches and target_matches:
-            source_node = source_matches[0]
-            target_node = target_matches[0]
-
-            link = graph.Graph.create_link({}, source_node, target_node)
-            log.info(link)
+    def _link_services(self, source_node, target_node):
+        link = graph.Graph.create_link({}, source_node, target_node)
+        if self._graph.get_link(link.id):
+            self._graph.update_link(link)
+        else:
             self._graph.add_link(link)
-
-    def _get_namespaces(self):
-        return [namespace['name'] for namespace in self._kiali_client.list_namespaces()]
