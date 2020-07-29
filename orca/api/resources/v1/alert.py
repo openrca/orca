889c45ffb2aa698e54bc4040aca9fd45c50a4d36
Aleksandra Galara <a.galara@samsung.com>
2020-07-29 14:33:01 +0200

    Add alerts endpoint(1)
    It introduces changes due
    to review. (#98)
    
    Signed-off-by: Aleksandra Galara <a.galara@samsung.com>

--- orca/api/resources/v1/alert.py
+++ orca/api/resources/v1/alert.py
@@ -14,39 +14,36 @@
 
 from flask_restx import Model, Namespace, Resource, fields, marshal
 
-node_fields = Model('Alert Node', {
+alerts_fields = Model('Alert Node', {
     'id': fields.String,
     'origin': fields.String,
-    'properties': {
-        'name': fields.String(attribute='properties.name'),
+    'name': fields.String(attribute='properties.name'),
+    'message': fields.String(attribute='properties.message'),
+    'severity': fields.String(attribute='properties.severity'),
+    'source': {
+        'origin': fields.String(attribute='properties.source_mapping.origin'),
         'kind': fields.String(attribute='properties.source_mapping.kind'),
-        'instance': fields.String(attribute='properties.source_mapping.properties.name'),
-        'namespace': fields.String(attribute='properties.source_mapping.properties.namespace', default="n/a"),
-	'message': fields.String(attribute='properties.message'),
-	'severity': fields.String(attribute='properties.severity')
-    }
-})
-
-alerts_fields = Model('Alerts', {
-    'alerts': fields.List(
-        fields.Nested(node_fields), attribute='nodes')
+        'properties': {
+            'name': fields.String(attribute='properties.source_mapping.properties.name'),
+            'namespace': fields.String(attribute='properties.source_mapping.properties.namespace', default="n/a")
+        }
+    },
+    'created_at': fields.String,
+    'updated_at': fields.String
 })
 
 
 class Alerts(Resource):
 
-    def __init__(self, api, alerts):
+    def __init__(self, api, graph):
         super().__init__()
-        self._alerts = alerts
+        self._alerts = graph
 
     def get(self):
-        data = {
-            'nodes': self._alerts.get_nodes(kind='alert'),
-        }
-        return marshal(data, alerts_fields)
+        return marshal(self._alerts.get_nodes(kind='alert'), alerts_fields)
 
 
-def initialize(alerts):
+def initialize(graph):
     api = Namespace('alerts', description='Alerts API')
-    api.add_resource(Alerts, '/', resource_class_args=[alerts])
+    api.add_resource(Alerts, '/', resource_class_args=[graph])
     return api
