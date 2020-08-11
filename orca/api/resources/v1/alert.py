3ad50b5703bd0439027a99c6c3afe9a1cd58c9b0
Aleksandra Galara <a.galara@samsung.com>
2020-08-11 11:24:06 +0200

    Add alerts endpoint(2)
    It introduces changes due
    to review. (#98)
    
    Signed-off-by: Aleksandra Galara <a.galara@samsung.com>

--- orca/api/resources/v1/alert.py
+++ orca/api/resources/v1/alert.py
@@ -14,20 +14,27 @@
 
 from flask_restx import Model, Namespace, Resource, fields, marshal
 
+
+properties_fields = Model('Properties', {
+    'name': fields.String(attribute='properties.source_mapping.properties.name'),
+    'namespace': fields.String(attribute='properties.source_mapping.properties.namespace', default="n/a")
+})
+
+
+source_fields = Model('Source', {
+    'origin': fields.String(attribute='properties.source_mapping.origin'),
+    'kind': fields.String(attribute='properties.source_mapping.kind'),
+    'properties': fields.Nested(properties_fields)
+})
+
+
 alerts_fields = Model('Alert Node', {
     'id': fields.String,
     'origin': fields.String,
     'name': fields.String(attribute='properties.name'),
     'message': fields.String(attribute='properties.message'),
     'severity': fields.String(attribute='properties.severity'),
-    'source': {
-        'origin': fields.String(attribute='properties.source_mapping.origin'),
-        'kind': fields.String(attribute='properties.source_mapping.kind'),
-        'properties': {
-            'name': fields.String(attribute='properties.source_mapping.properties.name'),
-            'namespace': fields.String(attribute='properties.source_mapping.properties.namespace', default="n/a")
-        }
-    },
+    'source': fields.Nested(source_fields),
     'created_at': fields.String,
     'updated_at': fields.String
 })
@@ -37,10 +44,10 @@ class Alerts(Resource):
 
     def __init__(self, api, graph):
         super().__init__()
-        self._alerts = graph
+        self._graph = graph
 
     def get(self):
-        return marshal(self._alerts.get_nodes(kind='alert'), alerts_fields)
+        return marshal(self._graph.get_nodes(kind='alert'), alerts_fields)
 
 
 def initialize(graph):
