4cd77444715644bd74b4646f1f3d5dfe51c910ae
aleksandra-galara <45719291+aleksandra-galara@users.noreply.github.com>
2020-08-17 22:27:08 +0200

    Add alerts endpoint (#99)
    Signed-off-by: Aleksandra Galara <a.galara@samsung.com>

--- orca/api/__init__.py
+++ orca/api/__init__.py
@@ -3,11 +3,12 @@ from flask_restx import Api
 
 from orca.api.resources.v1 import graph as graph_ns
 from orca.api.resources.v1 import ingestor as ingestor_ns
-
+from orca.api.resources.v1 import alert as alert_ns
 
 def initialize(graph):
     blueprint = Blueprint('api', __name__, url_prefix='/v1')
     api = Api(blueprint, title='ORCA API', version='0.1',)
     api.add_namespace(graph_ns.initialize(graph))
     api.add_namespace(ingestor_ns.initialize(graph))
+    api.add_namespace(alert_ns.initialize(graph))
     return blueprint
