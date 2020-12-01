2124f8a2a25fb05b628c933fa664978fba1862e3
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2020-12-01 20:58:51 +0100

    Fix invalid Flask request import
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- orca/api/apis/v1/graph.py
+++ orca/api/apis/v1/graph.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask_restx import Namespace, Resource, request
+from flask import request
+from flask_restx import Namespace, Resource
 
 from orca.api.schema import GraphSchema, GraphQuerySchema
 
